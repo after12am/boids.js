a3038a5df08f7ce8aac013ce4e98e2de45ebb1b0
satoshi okami <me.after.12am@gmail.com>
2012-03-25 16:46:33 +0900

    change copyright

--- build/build.py
+++ build/build.py
@@ -14,32 +14,9 @@ def deleteComment(str):
 	return re.sub(p, "", str)
 
 def main(p):
-	content = "/* summarized content */\n"
-	content += """
-/********************************************************************************** 
-
- Copyright (C) 2012 satoshi okami
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of
- this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to
- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- of the Software, and to permit persons to whom the Software is furnished to do
- so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
-
- **********************************************************************************/
-"""
+	content = "// github -> https://github.com/after12am/boids.js\n"
+	content += "\n"
+	content += "/* summarized content */\n"
 	
 	for p in paths:
 		c = get_file_content(p)
